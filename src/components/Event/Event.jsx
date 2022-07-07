67c56e312a82d5f631024cb260786428e5b4a91e
Denis <abcdenis95@gmail.com>
2022-07-07 11:43:14 +0300

    add

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import { FaMapMarkerAlt, FaUserAlt, FaCalendarAlt, FaClock } from 'react-icons/fa';
+import { formatEventStart, formatEventDuration } from "helper"
+import { Card,EventName,EventInfo,Chip } from './Event.styled';
+import css from './Event.module.css';
+
+
+export const Event = ({ name, location, speaker, type, start, end }) => {
+  const formattedStart = formatEventStart(start);
+  const duration = formatEventDuration(start, end);
+
+  return (
+    <Card >
+      <EventName>{name}</EventName>
+      <EventInfo >
+        <FaMapMarkerAlt/>
+        {location}
+      </EventInfo>
+      <EventInfo >
+        <FaUserAlt/>
+        {speaker}
+      </EventInfo>
+      <EventInfo>
+        <FaCalendarAlt/>
+        {formattedStart}
+      </EventInfo>
+      <EventInfo>
+        <FaClock/>
+        {duration}
+      </EventInfo>
+      <Chip type={type}>{type}</Chip>
+    </Card>
+  );
+};
+
+// className={`${css.chip} ${css[type]}`}
+
+Event.propTypes = {
+  name: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  speaker: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  start: PropTypes.string.isRequired,
+  end: PropTypes.string.isRequired,
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/App/App';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
+
+const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    green: '#4caf50',
+    red: '#f44336',
+    blue: '#2196f3',
+    primaryText: '#212121',
+    secondaryText: '#757575',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 );
